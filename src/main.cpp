526bfef6c7298c73456b9f4a93fa5825326cc986
jkfuhs <fuhs.jk@gmail.com>
2025-03-06 16:44:21 -0800

    resolution toggle

--- src/main.cpp
+++ src/main.cpp
@@ -27,8 +27,11 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+// const unsigned int SCR_WIDTH = 800;
+// const unsigned int SCR_HEIGHT = 600;
+
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
 
 class Application : public EventCallbacks
 {
